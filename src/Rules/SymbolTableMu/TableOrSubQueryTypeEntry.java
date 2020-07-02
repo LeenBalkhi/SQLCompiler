d19ec4ffc53ecc55b68aabee309d82d56f4464b4
muharej <51271279+muharej@users.noreply.github.com>
2020-07-02 16:15:34 +0300

    final

--- src/Rules/SymbolTableMu/TableOrSubQueryTypeEntry.java
+++ src/Rules/SymbolTableMu/TableOrSubQueryTypeEntry.java
@@ -1,24 +1,19 @@
 package Rules.SymbolTableMu;
 
-import java.util.ArrayList;
-
 public class TableOrSubQueryTypeEntry {
-    public ArrayList<TableOrSubQueryInnerTypeEntry> types = new ArrayList<>();
+    public SqlType sqlType = new SqlType();
     public String as;
 
-//    public String findType(String name) {
-//        for(int i=0;i<sqlType.entries.size();i++){
-//            if(sqlType.entries.get(i).name.equals(name))
-//                return sqlType.entries.get(i).type;
-//        }
-//        return null;
-//    }
+    public String findType(String name) {
+        for(int i=0;i<sqlType.entries.size();i++){
+            if(sqlType.entries.get(i).name.equals(name))
+                return sqlType.entries.get(i).type;
+        }
+        return null;
+    }
 
     public void printType(){
-        System.out.println("Outer Type "+as+":");
-        for(int i=0;i<types.size();i++){
-            System.out.println("Inner Type "+types.get(i).as+":");
-            types.get(i).sqlType.printType();
-        }
+        System.out.println();
+        sqlType.printType();
     }
 }
