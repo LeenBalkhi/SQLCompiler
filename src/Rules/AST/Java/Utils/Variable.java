5049233078fdc8e03a78f47a6f041f2db92dfc8d
muharej <51271279+muharej@users.noreply.github.com>
2020-07-02 21:24:18 +0300

    final

--- src/Rules/AST/Java/Utils/Variable.java
+++ src/Rules/AST/Java/Utils/Variable.java
@@ -3,6 +3,7 @@ import Rules.AST.Node;
 import Rules.SymbolTableMu.Scope;
 import Rules.SymbolTableMu.Symbol;
 import Rules.Utils.Error;
+import Rules.Utils.Warning;
 
 
 public class Variable extends Node {
@@ -21,7 +22,7 @@ public class Variable extends Node {
             if( symbol == null)
                 throw new Error(0,0,"Variable " + ((SimpleVariable)variable).VariableName.get(0) + " Does Not Exist");
             else if(symbol.type == null){
-                throw new Error(0,0,"Variable " + ((SimpleVariable)variable).VariableName.get(0) + " Has Not Been Given A Value Yet");
+                throw new Warning(0,0,"Variable " + ((SimpleVariable)variable).VariableName.get(0) + " Has Not Been Given A Value Yet");
             }
             else return symbol.type;
         }
